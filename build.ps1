4e8715a1ecf07466f89acba3acb2390a7282f114
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-08-03 23:24:30 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 ﻿#Remove-Item –path ./ –recurse
 & java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/convert -l python -c packageconfig.json
-#(Get-Content ./client/package.json).replace('v1', '1.0.1') | Set-Content ./client/package.json
+(Get-Content ./cloudmersive_convert_api_client/api_client.py).replace((Get-Content ./core/orig.py), (Get-Content ./core/replace.py)) | Set-Content ./cloudmersive_convert_api_client/api_client.py
