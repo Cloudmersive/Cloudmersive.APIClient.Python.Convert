29f00a6d5f07853d20f20114a0efe60c2dc4639a
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-08-03 23:51:46 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,5 +1,8 @@
 ﻿#Remove-Item –path ./ –recurse
 & java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/convert -l python -c packageconfig.json
+
+# Bug fix
+
 $newcontent = (Get-Content ./core/replace.py -Raw)
 Write-Host $newcontent
 (Get-Content ./cloudmersive_convert_api_client/api_client.py).replace('if response_type:', $newcontent) | Set-Content ./cloudmersive_convert_api_client/api_client.py
