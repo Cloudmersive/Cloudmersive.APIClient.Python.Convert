e9473dfc9d34af54c0bf7d06771523f41da5735e
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-09-02 12:28:47 -0700

    Added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 """
     convertapi
 
-    Convert API lets you effortlessly convert file formats and types.  # noqa: E501
+    
 
     OpenAPI spec version: v1
     
@@ -14,13 +14,16 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "cloudmersive_convert_api_client"
-VERSION = "2.0.4"
+VERSION = "2.0.5"
 # To install the library, run the following
 #
 # python setup.py install
 #
 # prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 REQUIRES = [
     "certifi>=2017.4.17",
@@ -35,12 +38,13 @@ setup(
     version=VERSION,
     description="convertapi",
     author_email="",
-    url="",
+    url="https://www.cloudmersive.com/convert-api",
     keywords=["Swagger", "convertapi"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    Convert API lets you effortlessly convert file formats and types.  # noqa: E501
-    """
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    
+
 )
